3fb2367e4e83a6c923d56aa68458768d1d676b87
Govind Jain <hello@govindjain.com>
2023-05-21 07:48:22 +0000

    Finalized Switch Logger App

--- switch_logger_app/layout_and_callbacks.py
+++ switch_logger_app/layout_and_callbacks.py
@@ -1,7 +1,7 @@
-from types import NoneType
 from dash import dcc, html
 from dash.dependencies import Input, Output, State
 from switch_logger_app.buffer_contents_display import buffer_contents_display
+
 
 # Write all the callbacks for this app inside the function
 def register_buffer_contents_callbacks(app, node_log):
@@ -9,39 +9,63 @@ def register_buffer_contents_callbacks(app, node_log):
         [Output('display-buffer-content', 'figure'),
          Output('selected-content', 'children'),
          Output('log-text', 'value'),
-        ],
-        [Input('go-button', 'n_clicks')],
-        [
-         State('clock-cycle-selector', 'value'),
+         ],
+        [Input('go-button', 'n_clicks')
+         ],
+        [State('clock-cycle-selector', 'value'),
          State('layer-selector', 'value'),
          State('switch-selector', 'value')
-        ],
+         ],
         prevent_initial_call=True
-        )
-    def display_buffer_contents(n_clicks, clock_cycle, layer_id, switch_id):
+    )
+    def display_buffer_contents(n_clicks, clock_cycle, layer_id, node_id):
         relevant_logs = ""
-        # print(node_log)
+
+        # Get the ID from "Node {ID}"
+        node_id = int(node_id.split(' ')[1])
+
         for log in node_log:
-            if clock_cycle == int(log['clock_cycle']) and layer_id == log['layer_id'] and switch_id == log['node_id']:
-                relevant_logs += "CLOCK_CYLCE={} LAYER_ID={} NODE_ID={} DIR_ID={} BUFFER_TYPE={} POS={} PACKET_ID={}\n".format(log["clock_cycle"],log["layer_id"],log["node_id"],log["dir_id"],log["buffer_type"],log['position_idx'],log['packet_id'])
+            same_layer = (int(layer_id) == int(log['layer_id']))
+            same_clock_cycle = (clock_cycle == int(log['clock_cycle']))
+            same_node_id = (node_id == int(log['node_id']))
+
+            if same_layer and same_clock_cycle and same_node_id:
+                relevant_logs += "CLOCK_CYCLE={} LAYER_ID={} NODE_ID={} DIR_ID={} BUFFER_TYPE={} POS={} PACKET_ID={}\n".format(
+                    log["clock_cycle"], log["layer_id"], log["node_id"], log["dir_id"], log["buffer_type"],
+                    log['position_idx'], log['packet_id'])
 
         if len(relevant_logs) == 0:
             relevant_logs = "No relevant logs are there for the selected options."
 
-        if layer_id is NoneType or switch_id is NoneType:
+        options_selected = True
+
+        if layer_id is None or node_id is None:
             relevant_logs = "Layer Id or Switch Id is not selected."
+            options_selected = False
 
-        selected = f'Relevant log files for the selected CLOCK_CYCLE: {clock_cycle}, LAYER_ID: {layer_id}, SWITCH_ID: {switch_id}'    
+        selected = f'Relevant log files for the selected CLOCK_CYCLE: {clock_cycle}, LAYER_ID: {layer_id}, NODE_ID: {node_id}'
 
-        fig = buffer_contents_display(node_log, clock_cycle, layer_id, switch_id)
+        fig = buffer_contents_display(node_log, clock_cycle, layer_id, node_id)
 
         if n_clicks is not None:
-            return fig, selected, relevant_logs
+            if options_selected is True:
+                return fig, selected, relevant_logs
+            else:
+                return None, selected, relevant_logs
 
 
-def get_switch_section_layout(layer_array, switch_array, max_clock_cycle):
+def get_switch_section_layout(layer_array, switch_array, max_clock_cycle, number_of_switches):
+    modified_node_list = []
+
+    for node in switch_array:
+        if int(node) < number_of_switches:
+            modified_node_list.append("Switch {}".format(node))
+        else:
+            modified_node_list.append("Repeater {}".format(node))
+
     switch_section_layout = [
         html.H2('Switch logger', style={'text-align': 'center', 'padding': '20px', 'font-size': '30px'}),
+        html.Div(id='output-data'),
         html.Div([
             html.Div([
                 html.Label('Select Layer: '),
@@ -50,13 +74,13 @@ def get_switch_section_layout(layer_array, switch_array, max_clock_cycle):
                     options=layer_array,
                     placeholder="Layer ID"
                 ),
-            ], style={'width': '200px', 'display': 'inline-block', 'verticalAlign': 'top', 'marginRight': '35px'}),  
+            ], style={'width': '200px', 'display': 'inline-block', 'verticalAlign': 'top', 'marginRight': '35px'}),
             html.Div([
-                html.Label('Select Switch: '),
+                html.Label('Select Node: '),
                 dcc.Dropdown(
                     id='switch-selector',
-                    options=switch_array,
-                    placeholder="Switch ID"
+                    options=modified_node_list,
+                    placeholder="Node ID"
                 ),
             ], style={'width': '200px', 'display': 'inline-block', 'verticalAlign': 'top', 'marginRight': '35px'}),
             html.Div([
@@ -78,7 +102,8 @@ def get_switch_section_layout(layer_array, switch_array, max_clock_cycle):
                 html.Div(id='selected-content'),
             ]),
             html.Div([
-                dcc.Textarea(id="log-text", value="Please select the above options...", style={'width': '75%', 'height': 200},)
+                dcc.Textarea(id="log-text", value="Please select the above options...",
+                             style={'width': '75%', 'height': 200}, )
             ]),
         ], style={'margin': '40px'}),
     ]
